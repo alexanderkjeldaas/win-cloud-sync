e76f5a023b861cb7559df995e9083589f6f67789
Alexander Kjeldaas <alexander@schibsted.com>
2019-03-05 19:46:12 +0100

    Initial impl.
    win-cloud-sync.ps1 downloads and executes lib.ps1
    
    lib.ps1 installs rclone and nssm

---
+++ lib.ps1
@@ -0,0 +1,111 @@
+#requires -version 3
+
+Add-Type -AssemblyName System.IO.Compression.FileSystem
+
+Function Unzip{
+    param([string]$zipfile, [string]$outpath)
+    [System.IO.Compression.ZipFile]::ExtractToDirectory($zipfile, $outpath)
+}
+
+Function Test-write{
+    param (
+        [Parameter(Mandatory=$True,Position=1)]
+        [string]$path)
+    $random = -join ((48..57) + (65..90) + (97..122) | Get-Random -Count 25 | % {[char]$_})
+    try{
+        "TESTWRITE" | Out-File -FilePath $path\$random -ErrorAction SilentlyContinue
+    } catch { }
+    $wrote = (Test-Path $path\$random -ErrorAction SilentlyContinue)
+    if ($wrote) {
+        Remove-Item $path\$random -ErrorAction SilentlyContinue;
+        return $true
+    } else {
+        return $false
+    }
+}
+
+Function Test-write-multiple{
+    param([string]$location, [string]$temp)
+    if (!(Test-write $location)){
+        write-warning "Unable to write to location.";
+        return $false
+    }
+    if (!(Test-write $temp)){
+        write-warning "Unable to write to temp location.";
+        return $false
+    }
+    return $true
+}
+
+# From https://gist.githubusercontent.com/justusiv/1ff2ad273cea3e33ca4acc5cab24c8e0/raw
+# but rewritten
+Function Install-Rclone{
+    param ([Parameter(Position=1)]
+           [string]$location="c:\windows\system32",
+           [Parameter(Mandatory=$false)]
+           [boolean]$exeonly=$true,
+           [string]$temp=$env:TEMP,
+           [boolean]$beta=$true)
+
+    if (!(Test-write-multiple $location $temp)){
+        return
+    }
+
+    $url = switch ( $beta ) {
+        $true  { "http://beta.rclone.org/rclone-beta-latest-windows-amd64.zip" }
+        $false { "http://downloads.rclone.org/rclone-current-windows-amd64.zip" }
+    }
+
+    $zipFile, $rcloneWindows = @("rclone-windows.zip", "rclone-windows") |
+        foreach {Join-Path -Path $temp -ChildPath $_}
+    Invoke-WebRequest -Uri $url -OutFile $zipFile
+
+    Unzip $zipFile $rcloneWindows
+
+    $allItems = (dir (dir $rcloneWindows).FullName)
+
+    $items = switch ($exeonly) {
+        $true  { $allItems | Where-Object {($_.name -eq "rclone.exe") -or ($_.name -eq "rclone")}}
+        $false { $allItems }
+    }
+
+    foreach ($item in $items) {
+        Move-Item $item.FullName -Destination $location -Force
+    }
+
+    @($rcloneWindows, $zipFile) | foreach {Remove-Item $_ -Recurse}
+
+    $rclone,$rcloneExe = @("rclone", "rclone.exe") | foreach {Join-Path -Path $location -ChildPath $_}
+    Move-Item $rclone $rcloneExe -Force -ErrorAction SilentlyContinue
+
+#    & $location\rclone.exe --version -q
+}
+
+
+Function install-nssm{
+    param ([Parameter(Position=1)]
+           [string]$location="c:\windows\system32",
+           [Parameter(Mandatory=$false)]
+           [boolean]$exeonly=$true,
+           [string]$temp=$env:TEMP,
+           [boolean]$beta=$true)
+
+    if (!(Test-write-multiple $location $temp)){
+        return
+    }
+
+    $url = "https://nssm.cc/ci/nssm-2.24-101-g897c7ad.zip"
+
+    $zipFile, $nssmWindows = @("nssm-windows.zip", "nssm-windows") |
+        foreach {Join-Path -Path $temp -ChildPath $_}
+    @($nssmWindows, $zipFile) | foreach {Remove-Item $_ -Recurse -Force -ErrorAction Ignore} 
+    Invoke-WebRequest -Uri $url -OutFile $zipFile
+
+    Unzip $zipFile $nssmWindows
+
+    $item = Join-Path -Path (dir (dir $nssmWindows).FullName | where {$_.name -eq "win64"}) -ChildPath "nssm.exe"
+
+    Move-Item $item -Destination $location -Force
+
+    @($nssmWindows, $zipFile) | foreach {Remove-Item $_ -Recurse}
+}
---
+++ win-cloud-sync.ps1
@@ -0,0 +1,69 @@
+#requires -version 3
+<#
+.SYNOPSIS
+  Installs the win-cloud-sync service to synchronize a local path with the cloud.
+
+.DESCRIPTION
+  This script will install a service called win-cloud-sync on your system."
+
+  There are two required components for this setup, namely rcloud and
+  nssm.
+
+  Rcloud is an utility that does the actual synchronizaiton of files
+  and directories, while nssm (The Non-Sucking Service Manager) installs,
+  configures, and runs rcloud at regular intervals.
+
+  win-cloud-sync is given a base directory and will by default do
+  the following:
+
+  $base\programs\rclone.exe         - the rclone binary
+  $base\programs\nssm.exe           - the nssm binary
+  $base\programs\win-cloud-sync.ps1 - this script
+  $base\data                        - data to synchronize
+
+.PARAMETER BasePath
+  The local path to install to and sync.
+
+.PARAMETER BasePath
+  The local path to install to and sync.
+
+.INPUTS
+  -LocalPath to install.
+  -exeonly If you only want to deal with the exe.
+  -Temp Location to use for temp.
+  -beta To download the latest beta
+
+.OUTPUTS
+  Outputs rclone version for verification
+
+.NOTES
+  Author:Alexander Kjeldaas
+
+.EXAMPLE
+
+  win-cloud-sync -location "d:\directory"
+  install-rclone -location "c:\rclone" -execonly $true -temp "c:\mytemp" -beta $true
+#>
+
+# This 
+# param ($ComputerName = $(throw "ComputerName parameter is required."))
+param (
+    [string]$server = "http://defaultserver",
+#    [string]$password = $(Read-Host "Input password, please"),
+    [switch]$force = $false
+)
+
+#[remote]
+#type = google cloud storage
+#client_id =
+#client_secret =
+#token = {"AccessToken":"xxxx.xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx-xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx","RefreshToken":"x/xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx_xxxxxxxxx","Expiry":"2014-07-17T20:49:14.929208288+01:00","Extra":null}
+#project_number = 12345678
+#object_acl = private
+#bucket_acl = private
+
+# write-host "This script will install a service called win-cloud-sync on your system."
+# write-host "The win-cloud-sync This script will install a service called win-cloud-sync on your system"
+
+$scriptUrl = "https://raw.githubusercontent.com/alexanderkjeldaas/win-cloud-sync/master/lib.ps1"
+Invoke-Expression ((Invoke-WebRequest -Uri $scriptUrl).content)
